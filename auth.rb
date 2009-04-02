eeae3e3089f76196be08d292b51401cd56a2277e
Matt Beedle <mattbeedle@googlemail.com>
2009-04-02 18:10:08 +0200

    auth template

---
+++ auth.rb
@@ -0,0 +1,20 @@
+load_template "http://github.com/mattbeedle/templates/raw/master/base_template.rb"
+
+gem 'authlogic',  :lib => 'authlogic'
+name = ask("What do you want your user to be called")
+generate :session, "#{name}_session"
+generate :model, name
+rake "gems:install"
+
+# TODO rspec controllers if using rspec
+generate :controller, "sessions"
+generate :controller, name.pluralize
+
+route "map.resource :session"
+if yes?("Use plural user resource")
+  route "map.resources #{name.pluralize.to_sym}"
+else
+  route "map.resource #{name.to_sym}"
+end
+
+git :add => ".", :commit => "-m 'adding authentication'"
