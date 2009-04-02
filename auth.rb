6168911ab2e709eb7c4792786ffd7007c71bb009
Matt Beedle <mattbeedle@googlemail.com>
2009-04-02 18:28:13 +0200

    order was wrong

--- auth.rb
+++ auth.rb
@@ -1,10 +1,10 @@
 load_template "http://github.com/mattbeedle/templates/raw/master/base.rb"
 
 gem 'authlogic',  :lib => 'authlogic'
+rake "gems:install"
 name = ask("What do you want your user to be called")
 generate :session, "#{name.downcase}_session"
 generate :model, name.downcase
-rake "gems:install"
 
 # TODO rspec controllers if using rspec
 generate :controller, "sessions"
@@ -12,9 +12,9 @@ generate :controller, name.downcase.pluralize
 
 route "map.resource :session"
 if yes?("Use plural user resource")
-  route "map.resources #{name.downcase.pluralize.to_sym}"
+  route "map.resources :#{name.downcase.pluralize}"
 else
-  route "map.resource #{name.downcase.to_sym}"
+  route "map.resource :#{name.downcase}"
 end
 
 git :add => ".", :commit => "-m 'adding authentication'"
