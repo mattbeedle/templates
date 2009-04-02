90c402abadf3adc6e4bd6c380a6f898790f2b428
Matt Beedle <mattbeedle@googlemail.com>
2009-04-02 18:24:40 +0200

    more changes

--- auth.rb
+++ auth.rb
@@ -2,19 +2,19 @@ load_template "http://github.com/mattbeedle/templates/raw/master/base.rb"
 
 gem 'authlogic',  :lib => 'authlogic'
 name = ask("What do you want your user to be called")
-generate :session, "#{name}_session"
-generate :model, name
+generate :session, "#{name.downcase}_session"
+generate :model, name.downcase
 rake "gems:install"
 
 # TODO rspec controllers if using rspec
 generate :controller, "sessions"
-generate :controller, name.pluralize
+generate :controller, name.downcase.pluralize
 
 route "map.resource :session"
 if yes?("Use plural user resource")
-  route "map.resources #{name.pluralize.to_sym}"
+  route "map.resources #{name.downcase.pluralize.to_sym}"
 else
-  route "map.resource #{name.to_sym}"
+  route "map.resource #{name.downcase.to_sym}"
 end
 
 git :add => ".", :commit => "-m 'adding authentication'"
