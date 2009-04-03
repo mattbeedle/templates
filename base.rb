80b896b12e6e2e1fd7937742755c0931a9bb9208
Matt Beedle <mattbeedle@googlemail.com>
2009-04-03 12:44:44 +0200

    now creates empty factories file

--- base.rb
+++ base.rb
@@ -17,9 +17,11 @@ if yes?("Do you want to use rspec")
   plugin "rspec",       :git => "git://github.com/dchelimsky/rspec.git"
   plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
   generate :rspec
+  touch "spec/factories.rb"
 else
   gem   'thoughtbot-shoulda',       :lib => "shoulda",        :source => "gems.github.com"
   gem   'seanhussey-woulda',        :lib => "woulda",         :source => "gems.github.com"
+  touch "test/factories.rb"
 end
 
 plugin "resource_controller", :git => "git://github.com/giraffesoft/resource_controller.git"
